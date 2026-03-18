2612830a21794bb225a6a3861eafd75f5e8c8090
Victor Gogo <victorgogo98@gmail.com>
2026-03-18 05:22:33 +0300

    style(auth): add missing semicolons to auth middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -23,10 +23,10 @@ const restrictTo = (...roles) => {
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
         message: 'You do not have permission to perform this action.'
-      })
+      });
     }
     next();
   }
 }
 
-module.exports = { protect, restrictTo }
+module.exports = { protect, restrictTo };
